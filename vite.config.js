22733f5a73d178fe290c6414af214a9a5615e95a
keijiek <kumatan800@gmail.com>
2023-01-09 22:19:35 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite';
+import autoprefixer from "autoprefixer";
+import path from "path";
+import glob from "glob";
+import { fileURLToPath } from "node:url";
+
+const rootValue       = path.resolve(__dirname, 'src');
+const publicDirValue  = path.resolve(__dirname, 'public');
+const outDirValue     = path.resolve(__dirname, 'dist');
+
+export default defineConfig({
+  root: rootValue,
+  base: './',
+  publicDir: publicDirValue,
+  css: {
+    // plugins: [autoprefixer],
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
+  build: {
+    outDir: outDirValue,
+    emptyOutDir: true,
+    minify: 'esbuild',// 値を false:boolean にするとminifyしない。規定値 'esbuild':string.
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob.sync(path.resolve(rootValue, "*.html")).map(file => [
+          path.relative(rootValue, file.slice(0, file.length - path.extname(file).length)),
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      // input: {
+      //  index: path.resolve(rootValue, "index.html"),
+      //  second: path.resolve(rootValue, "second.html"),
+      // },
+      output: {
+      },
+    },
+  },
+  plugins:[],
+  resolve: {
+    alias: {
+      // '~bootstrap': path.resolve(__dirname,'node_modules/bootstrap'),
+    }
+  }
+});
