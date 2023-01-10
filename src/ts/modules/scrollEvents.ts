efe489721624c2a05a9f7702c7d38aa2fcf7f825
keijiek <kumatan800@gmail.com>
2023-01-10 09:42:13 +0900

    morning 10th Jan. 2023

--- src/ts/modules/scrollEvents.ts
+++ src/ts/modules/scrollEvents.ts
@@ -1,17 +1,50 @@
-
+/**
+ * スクロールにより現れたり消えたりするポリシーの各項目
+ */
 class PolicyItem {
   protected element:HTMLElement;
-  
-  constructor(element:HTMLElement) {
+  protected visibilityStartPoint:number;
+  protected visibilityEndPoint:number;
+  private fadeClassName : string;
+
+  constructor(element:HTMLElement, visibleStart:number, visibleEnd:number, fadeClassName:string) {
     this.element = element;
+    this.visibilityStartPoint = visibleStart;
+    this.visibilityEndPoint = visibleEnd;
+    this.fadeClassName = fadeClassName;
   }
 
-  setVisibleArea():PolicyItem {
-    
+  private isInVisibleArea(scrollAmount:number):boolean {
+    if(scrollAmount>this.visibilityStartPoint && scrollAmount<this.visibilityEndPoint) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  visibleCheck(scrollAmount:number):PolicyItem {
+    if(this.isInVisibleArea(scrollAmount)) {
+      this.element.classList.add(this.fadeClassName);
+    } else {
+      this.element.classList.remove(this.fadeClassName);
+    }
     return this;
   }
 }
 
-export default function scrollEvents() {
+/**
+ * 各境界線(X軸に平行)のY座標を算出する。
+ */
+class BoundaryAllocator {
+  constructor(itemArray:Array<HTMLElement>) {
+    
+  }
+  
+}
 
+export default function scrollEvents(fadeItemClassName:string, fadeupClassName:string):void {
+
+  const scrollAmount = window.pageYOffset;
+  const fadeItems = document.getElementsByClassName(fadeItemClassName);
+  
 };
